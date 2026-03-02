f69b027e7276b2200008715cece993f06e62cbda
Victor M. Rivas <vrivas@ujaen.es>
2026-03-02 17:22:29 +0100

    Paso 1. Creo las clases Producto y ProductoTest vacías.

---
+++ src/main/java/es/vrivas/dagil/Producto.java
@@ -0,0 +1,9 @@
+package es.vrivas.dagil;
+
+/**
+ * Descripción de la clase producto.
+ * @author Victor Rivas <vrivas@ujaen.es>
+*/
+public class Producto {
+
+}
---
+++ src/test/java/es/vrivas/dagil/ProductoTest.java
@@ -0,0 +1,12 @@
+package es.vrivas.dagil;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests para la clase ProductoTest
+ */
+public class ProductoTest {
+
+}
